d30d5edad6b386adaf00df80a7b611b2fcc56731
rach-dot <rachael.holmes20@gmail.com>
2021-01-27 22:33:05 +1300

    Fourth commit.

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println ("Hello my name is Rachael");
 		System.out.println ("My favourite colour is green");
 		System.out.println ("My favourite mode of transportation is walking");
+		System.out.println ("The best day of the week is Saturday");
 	}
 
 }
